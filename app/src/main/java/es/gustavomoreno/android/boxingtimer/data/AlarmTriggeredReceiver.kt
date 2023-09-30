b460b67ecf4fa551b4be8db66707feae45cfc862
gustamor <gustavo.moreno.fernandez@gmail.com>
2023-09-30 17:18:05 +0200

    added bells

--- app/src/main/java/es/gustavomoreno/android/boxingtimer/data/AlarmTriggeredReceiver.kt
+++ app/src/main/java/es/gustavomoreno/android/boxingtimer/data/AlarmTriggeredReceiver.kt
@@ -3,9 +3,11 @@ package es.gustavomoreno.android.boxingtimer.data
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
 import android.util.Log
 import android.widget.Toast
 import dagger.hilt.android.AndroidEntryPoint
+import es.gustavomoreno.android.boxingtimer.R
 
 
 @AndroidEntryPoint
@@ -14,8 +16,13 @@ class AlarmTriggeredReceiver : BroadcastReceiver() {
         const val REQUEST_CODE: Int = 1
     }
 
+    private var mediaPlayer: MediaPlayer? = null
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == "ALARM_STARTED") {
+            mediaPlayer = MediaPlayer.create(context, R.raw.bell_3 )
+
+            mediaPlayer?.seekTo(0)
+            mediaPlayer?.start()
             Log.d("Sec", "Alarm started")
             Toast.makeText(context, "Alarm started!", Toast.LENGTH_SHORT).show()
         }
